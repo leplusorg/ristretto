f9fbb95749908c6d9dd9c9145dab3e82e0b1f556
Thomas Leplus <thomas.leplus@gmail.com>
2016-10-18 23:15:53 -0700

    Adding identity tests

---
+++ src/test/java/org/leplus/util/TestIdentityObject.java
@@ -0,0 +1,75 @@
+package org.leplus.util;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import org.junit.Test;
+
+public class TestIdentityObject {
+
+	@SuppressWarnings("unchecked")
+	public static <T extends Serializable> T deepClone(final T object) throws IOException, ClassNotFoundException {
+		ObjectInputStream ois = null;
+		ObjectOutputStream oos = null;
+		try {
+			final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(bos);
+			oos.writeObject(object);
+			oos.flush();
+			final ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray());
+			ois = new ObjectInputStream(bin);
+			return (T) ois.readObject();
+		} finally {
+			if (ois != null) {
+				ois.close();
+			}
+			if (oos != null) {
+				oos.close();
+			}
+		}
+	}
+
+	@Test
+	public void testEqual() {
+		assertTrue(IdentityObject.IT == IdentityObject.IT);
+	}
+
+	@Test
+	public void testEqualClone() {
+		assertTrue(IdentityObject.IT == IdentityObject.IT.clone());
+	}
+
+	@Test
+	public void testEqualDeepClone() throws ClassNotFoundException, IOException {
+		assertTrue(IdentityObject.IT == deepClone(IdentityObject.IT));
+	}
+
+	@Test
+	public void testEquals() {
+		assertTrue(IdentityObject.IT.equals(IdentityObject.IT));
+	}
+
+	@Test
+	public void testEqualsClone() {
+		assertTrue(IdentityObject.IT.equals(IdentityObject.IT.clone()));
+		assertTrue(IdentityObject.IT.clone().equals(IdentityObject.IT));
+	}
+
+	@Test
+	public void testEqualsDeepClone() throws ClassNotFoundException, IOException {
+		assertTrue(IdentityObject.IT.equals(deepClone(IdentityObject.IT)));
+		assertTrue(deepClone(IdentityObject.IT).equals(IdentityObject.IT));
+	}
+
+	@Test
+	public void testHashCode() {
+		assertTrue(IdentityObject.IT.hashCode() == IdentityObject.IT.hashCode());
+	}
+	
+}
