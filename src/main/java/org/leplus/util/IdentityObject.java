c223fee38a4f2105d97d62d52beddb0b69f03763
Thomas Leplus <thomas.leplus@gmail.com>
2016-10-18 22:40:49 -0700

    Keep singleton after serialization

--- src/main/java/org/leplus/util/IdentityObject.java
+++ src/main/java/org/leplus/util/IdentityObject.java
@@ -5,31 +5,35 @@ import java.io.Serializable;
 public final class IdentityObject implements Cloneable, Serializable {
 
 	private static final long serialVersionUID = Long.MAX_VALUE;
-	
+
 	public static final IdentityObject IT = new IdentityObject();
-	
+
 	private IdentityObject() {
 		super();
 	}
-	
-	@Override
-	public int hashCode() {
-		return Integer.MAX_VALUE;
-	}
-	
+
 	@Override
 	public IdentityObject clone() {
-		return this;
-	}
-	
-	@Override
-	public String toString() {
-		return getClass().getName();
+		return IT;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		return obj instanceof IdentityObject;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return Integer.MAX_VALUE;
+	}
+
+	private Object readResolve() {
+		return IT;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getName();
+	}
+
 }
